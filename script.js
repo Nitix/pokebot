1a56c7b6f5e9f61f1bc19b9f0739bdbade83f5ae
Guillaume Pierson <guillaumep@marmelab.com>
2023-11-08 22:14:06 +0100

    A*

--- script.js
+++ script.js
@@ -196,6 +196,11 @@ let configureDungeon = () => {
   };
 
   const goToTile = (X, Y) => {
+    const next = aStarAlgorithm({ x: positionX, y: positionY }, { x: X, y: Y });
+    if (next) {
+      X = next.x;
+      Y = next.y;
+    }
     if (Y < positionY) {
       moveUp();
       return true;
@@ -328,6 +333,118 @@ let configureDungeon = () => {
 
     DungeonRunner.handleInteraction();
     requestAnimationFrame(move);
+  };
+
+  /**
+   * A* algorithm
+   * @param {Object} start
+   * @param {number} start.x
+   * @param {number} start.y
+   * @param {Object} end
+   * @param {number} end.x
+   * @param {number} end.y
+   */
+  const aStarAlgorithm = (start, end) => {
+    try {
+      if (start.x === end.x && start.y === end.y) {
+        console.log("Already on the tile");
+        return null;
+      }
+      let openSet = [
+        {
+          x: start.x,
+          y: start.y,
+          parent: null,
+          gScore: 0,
+          fScore: distance(start, end),
+        },
+      ];
+      while (openSet.length) {
+        let current = openSet.reduce((prev, current) => {
+          if (!prev) {
+            return current;
+          }
+          if (current.fScore < prev.fScore) {
+            return current;
+          }
+          return prev;
+        }, null);
+        if (current.x === end.x && current.y === end.y) {
+          let path = [current];
+          while (current.parent) {
+            path.push(current.parent);
+            current = current.parent;
+          }
+          return path[path.length - 2];
+        }
+        openSet = openSet.filter((e) => e !== current);
+        const neighbors = [
+          { x: current.x - 1, y: current.y },
+          { x: current.x + 1, y: current.y },
+          { x: current.x, y: current.y - 1 },
+          { x: current.x, y: current.y + 1 },
+        ];
+        for (const neighbor of neighbors) {
+          if (
+            neighbor.x < 0 ||
+            neighbor.x >= size ||
+            neighbor.y < 0 ||
+            neighbor.y >= size
+          ) {
+            continue;
+          }
+          if (neighbor.x === current.x && neighbor.y === current.y) {
+            continue;
+          }
+          const gScore =
+            current.gScore + tileGScore(`${neighbor.x},${neighbor.y}`);
+          const fScore = gScore + distance(neighbor, end);
+          const existing = openSet.find(
+            (e) => e.x === neighbor.x && e.y === neighbor.y
+          );
+          if (existing) {
+            if (existing.gScore > gScore) {
+              existing.gScore = gScore;
+              existing.fScore = fScore;
+              existing.parent = current;
+            }
+          } else {
+            openSet.push({
+              x: neighbor.x,
+              y: neighbor.y,
+              parent: current,
+              gScore,
+              fScore,
+            });
+          }
+        }
+      }
+    } catch (e) {
+      console.error(e.message);
+      return null;
+    }
+  };
+
+  const distance = (start, end) => {
+    return Math.abs(start.x - end.x) + Math.abs(start.y - end.y);
+  };
+
+  const tileGScore = (id) => {
+    const [x, y] = id.split(",");
+    const board = document.querySelector(".dungeon-board > tbody:nth-child(1)");
+    const row = board.children[y];
+    const tile = row.children[x];
+    if (!tile) {
+      console.log("Not found", id, x, y);
+      return 1_000_000;
+    }
+    if (tile.classList.contains("tile-invisible")) {
+      return 10;
+    }
+    if (tile.classList.contains("tile-enemy")) {
+      return 100;
+    }
+    return 1;
   };
 
   requestAnimationFrame(move);
