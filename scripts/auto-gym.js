a38e4caee0e1ff99a89df8bc5c8d8c88d1643dda
Guillaume Pierson <guillaumep@marmelab.com>
2024-01-21 12:18:32 +0100

    Fix button not appearing after pokéclicker update (#19)

--- scripts/auto-gym.js
+++ scripts/auto-gym.js
@@ -1,15 +1,18 @@
 class AutoGym {
   static custom = false;
+  static options = AutoGym.loadDefaultConfig();
 
   static start() {
     if (AutoGym.custom) {
       return;
     }
+    AutoGym.loadDefaultConfig(true);
     GymRunner.originalGymWon = GymRunner.gymWon;
     GymRunner.gymWon = (gym) => {
       GymRunner.originalGymWon(gym);
       GymRunner.startGym(gym);
     };
+    AutoGym.optimizeConfigs();
     AutoGym.custom = true;
     return true;
   }
@@ -17,9 +20,47 @@ class AutoGym {
   static stop() {
     GymRunner.gymWon = GymRunner.originalGymWon;
     AutoGym.custom = false;
+    AutoGym.restoreDefaultConfig();
   }
 
   static isRunning() {
     return AutoGym.custom;
   }
+
+  static loadDefaultConfig(setImmediately = false) {
+    const options = {
+      showGymGoAnimation:
+        Settings.getSetting("showGymGoAnimation").value ?? true,
+      showCurrencyGainedAnimation:
+        Settings.getSetting("showCurrencyGainedAnimation").value ?? true,
+      showCurrencyLostAnimation:
+        Settings.getSetting("showCurrencyLostAnimation").value ?? true,
+    };
+    if (setImmediately) {
+      AutoGym.options = options;
+    }
+    return options;
+  }
+
+  static restoreDefaultConfig() {
+    Settings.getSetting("showGymGoAnimation").set(
+      AutoGym.options.showGymGoAnimation
+    );
+    Settings.getSetting("showCurrencyGainedAnimation").set(
+      AutoGym.options.showCurrencyGainedAnimation
+    );
+    Settings.getSetting("showCurrencyLostAnimation").set(
+      AutoGym.options.showCurrencyLostAnimation
+    );
+  }
+
+  static optimizeConfigs() {
+    Settings.getSetting("showGymGoAnimation").set(false);
+    Settings.getSetting("showCurrencyGainedAnimation").set(false);
+    Settings.getSetting("showCurrencyLostAnimation").set(false);
+  }
 }
+
+window.addEventListener("beforeunload", () => {
+  AutoGym.restoreDefaultConfig();
+});
